627b126e4b41b6183266f55eb89a9483956facfe
fstrxd <m.oney.gold@mail.ru>
2024-03-13 22:23:18 +0600

    13.03

---
+++ js/let_var_const.js
@@ -0,0 +1,54 @@
+// let x1 = "мир"
+// alert(`Привет "${x1}"`)
+/*
+//dfffasdfasdda
+/*
+hgfd
+
+sadfasdfsdf
+sdafasdfasdf
+asdfasdfasdf
+asdfasdfasdf
+  */
+// var x = 12;
+// let a = 10;
+// let b = "Omsk";
+// console.log(x % a, b + (x % a));
+// let msg;
+// msg = 123;
+// msg = 456;
+// msg = 789;
+// console.log(msg);
+// 0;
+
+// let z = 1,
+//   v = 2,
+//   c = 3;
+// console.log(b + v +" # "+ c);
+
+let a = 10;
+let b = 5;
+let c = a;
+let d = b - c; 
+console.log(d, a);
+
+ a = 5;
+ b = 10;
+ c = a;
+ d = b - c; 
+console.log(d, a);
+
+let _x = c - d
+let фва = 1
+console.log(фва);
+
+const birthday = "14.10.10"
+const ID = 1
+console.log(birthday);
+
+
+
+let admin;
+let name = "Yaroslav"
+admin = name;
+alert(admin)  
---
+++ js/types.js
@@ -0,0 +1,32 @@
+let word = 'Omsk' //string
+let x = 123 //number
+let z = true //boolean
+let c = {} //object
+let v = undefined //undefined
+
+console.log(typeof word );
+let o = {
+    name: "yarik",
+    age: 13
+}
+
+console.log(Number(o.name));
+
+const keys = {
+    $1: 1,
+    $2: 2
+}
+keys.$1 = 3
+console.log(keys.$1);
+
+
+
+let k = "55123"
+console.log((String(x)));
+
+let l = 1000
+
+for(let r = 0; l <= 0; r++ ){
+console.log(l - 7);
+}
+
